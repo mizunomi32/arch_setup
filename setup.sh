788cd57d977909545dcb82f3383cb75aaa160c17
mizunomi <miccchi@outlook.jp>
2017-02-03 11:10:32 +0900

    editable

--- setup.sh
+++ setup.sh
@@ -7,15 +7,33 @@ set -ux
 mkdir arch_setup
 cd arch_setup
 wget https://mizunomi32.github.io/arch_setup/01core_setup.sh
-./01core_setup.sh
 set +x
+echo -n "Do you edit 01core_setup.sh X? (y/n)"
+read edityn
+case $edityn in
+  "y" )
+    vim 01core_setup.sh
+    edityn="n"
+    ;;
+esac
+./01core_setup.sh
 
-case needx in
+
+case $needx in
   "y")
     set -ux
     wget https://mizunomi32.github.io/arch_setup/02x_setup.sh
-    ./02x_setup.sh
     set +x
+    echo -n "Do you edit 02x_setup.sh X? (y/n)"
+    read edityn
+    case $edityn in
+      "y" )
+        vim 02x_setup.sh
+        edityn="n"
+        ;;
+    esac
+    ./02x_setup.sh
+
     ;;
 esac
 echo "-----------------------"
