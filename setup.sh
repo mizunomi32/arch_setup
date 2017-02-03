fb3a3fe37eb5fd91b50c1f4be443fb241126acbc
mizunomi <miccchi@outlook.jp>
2017-02-03 09:34:41 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
+
+echo -n "Do you need X? (y/n)"
+read needx
+
 set -ux
-
-
-
+mkdir arch_setup
+cd arch_setup
+wget https://mizunomi32.github.io/arch_setup/01core_setup.sh
+./01core_setup.sh
 set +x
+
+case needx in
+  "y")
+    set -ux
+    wget https://mizunomi32.github.io/arch_setup/02x_setup.sh
+    ./02x_setup.sh
+    set +x
+    ;;
+esac
+echo "-----------------------"
+echo "ALL install finished."
+echo "-----------------------"
